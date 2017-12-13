13188f3962c1eeacd98678d507dce7a8a1418e79
camilabelen <ccornejoss@gmail.com>
2017-12-12 23:39:06 -0300

    segunda vista y primerade sign up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
-
+$(document).ready(function(){
+  $('.signUp').hide();
+  $('.splash').show();
+  splash(3000);
+  //funcion pagina splash
+  function splash(time){
+    setTimeout(function(){
+    $('.splash').fadeOut(); 
+    }, time);
+    $('.signUp').delay(3500).fadeIn();
+  }  
+  
+});
 
  
